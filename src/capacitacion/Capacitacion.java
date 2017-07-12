43cd6997e375cf9f6f467bfc9e5da1bc45934b0c
p.orientacion <p.orientacion@S2-EQ17.uao.edu.co>
2017-07-12 15:00:07 -0500

    Crear repositorio

---
+++ src/capacitacion/Capacitacion.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package capacitacion;
+
+/**
+ *
+ * @author p.orientacion
+ */
+public class Capacitacion {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i=0; i<100; i++)
+        {
+            System.out.println("Voy en "+(i+1));
+        }
+    }
+    
+}
